d342761f2c16c8ffa71acd862ac9bb691c6deb4b
Taylor Lang <lang.taylor.j@gmail.com>
2017-03-01 20:41:54 -0600

    trying to include backbone

--- js/script.js
+++ js/script.js
@@ -22,12 +22,12 @@ var handleCurrentWeatherResponse = function(weatherObject) {
 	// console.log(windSpeed)
 	var finalWeatherHTML = ''
 	var containerNode = document.querySelector('.weatherContainer')
-	finalWeatherHTML += renderHTML(weatherObject)
+	finalWeatherHTML += renderCurrentHTML(weatherObject)
 	containerNode.innerHTML = finalWeatherHTML
 
 }
 
-var renderHTML = function(weatherObject) {
+var renderCurrentHTML = function(weatherObject) {
 	var getHTML = ''
 	getHTML += '<h1 class="temp">' + weatherObject.currently.temperature + '&#8457' + '</h1>'
 	getHTML += '<p class="windSpeed">' + 'The current wind speed is ' + weatherObject.currently.windSpeed + 'mph' + '</p>'
@@ -48,63 +48,36 @@ var makeNavString = function(lat,lng) {
 	// stick them onto the DOM
 	//refer to amanda's slack
 }
-var hashController = function() {
-	console.log('hashController')
-	// hash must have form #LAT/LNG/VIEWTYPE
-	// read the latitude, longitude, and viewtype from the hash
+var WeatherRouter = Backbone.Router.extend({
+	
+	routes: {
+		"current" : "showCurrentPage",
+		"hourly" : "showHourlyPage",
+		"daily" : "showDailyPage",
+		":lat/:lng/:viewType": "showWeatherStuff",
+		"*default" : "redirectToHome"
+	},
 
-		// the lat and long should be used to construct an api request url
-		// make that request, store the promise.
+	redirectToHome: function() {
+		location.hash = 'current'
+	},
 
-		// depending on the viewtype, assign one of three view rendering functions
-		// to run when the response comes back
-
-
-	var hashString = location.hash.substr(1),
-		hashParts = hashString.split('/'),
-		lat = hashParts[0],
-		lng = hashParts[1],
-		viewType = hashParts[2]
-
-
-	makeNavString(lat,lng)
-
-	if (hashParts.length < 3) {
-		console.log('<3')
-		navigator.geolocation.getCurrentPosition(onCurrentPositionFound, onCurrentPositionNotFound)
-		// redirect the user. tbd later.
-		// default behavior. run a function to get current position, writing the 
-			// current lat and long to the hash after some delay. let the controller
-			// kick back into action at that point.
-		return 
-	}
-
-	var requestURL = baseURL + '/' + lat + ',' + lng + '?callback=?'
-
-
-	if (viewType === 'current')  {
-		var weatherPromise = $.getJSON(requestURL)
+	showCurrentPage: function() {
+		var weatherPromise = $.getJSON('https://api.darksky.net/forecast/f693ad4fa47137321f70f403e91be488/37.8267,-122.4233')
 		weatherPromise.then(handleCurrentWeatherResponse)
-		// queue up the currentRenderer
-		console.log('showing current data')
+		renderCurrentHTML()
+	},
+
+	showHourlyPage: function() {
+		renderHourlyPage()
+	},
+	showDailyPage: function() {
+		renderDailyPage()
+	},
+	showWeatherStuff: function(lat, lng, viewType) {
+		renderSearchPage()
 	}
+})
 
-	else if (viewType === 'daily') {
-		var weatherPromise = $.getJSON(requestURL)
-		weatherPromise.then(handleDailyWeatherResponse)
-		// queue up the dailyRenderer
-		alert('showing daily data')
-
-	}
-
-	else if (viewType === 'hourly') {
-		var weatherPromise = $.getJSON(requestURL)
-		weatherPromise.then(handleHourlyWeatherResponse)
-		//queue up the hourly renderer
-		alert('showing hourly data')
-	}
-}
-
-window.addEventListener('hashchange',hashController)
-
-hashController()
+var rtr = new WeatherRouter()
+Backbone.history.start()
